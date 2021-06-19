3c3cb8ee80ea332bd473f0eab07076e11a51fa3d
NazmieHasan <58294359+NazmieHasan@users.noreply.github.com>
2021-06-19 15:50:51 +0300

    add autoplaySpeed in slick slider

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -1,7 +1,6 @@
 $('.project-slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
     dots: true,
-    arrows: true
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000
 });
